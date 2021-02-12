05bbc9e9cff84dc9f6b352de089d0fbb7a06ffe2
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2021-02-12 18:52:02 +0100

    Routes & Templates

---
+++ django/first_project/my_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    # index - function from views.py
+    path("test", views.test, name="test"),
+    path("david", views.david, name="david"),
+    path("brian", views.brian, name="brian"),
+    path("indexStyle", views.indexStyle, name="indexStyle"),
+    path("indexTemplate", views.indexTemplate, name="indexTemplate"),
+    path("<str:name>", views.greet, name="greet"),
+]
--- django/first_project/my_app/views.py
+++ django/first_project/my_app/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello, world!")
+
+
+def test(request):
+    return HttpResponse("Test")
+
+
+def david(request):
+    return HttpResponse("Hello David")
+
+
+def brian(request):
+    return HttpResponse("Hello Brian")
+
+
+""" def greet(request, name):
+    return HttpResponse(f"Hello, {name}!") """  # {name.capitalize()}
+
+# updated
+
+
+def greet(request, name):
+    return render(request, "my_app/greet.html", {"name": name.capitalize()})
+
+
+def indexStyle(request):
+    return HttpResponse("<h1 style=\"color:blue\">Hello, world!</h1>")
+
+
+""" TEMPLATES """
+
+
+def indexTemplate(request):
+    return render(request, "my_app/index.html")
