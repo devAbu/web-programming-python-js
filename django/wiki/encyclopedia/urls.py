6774f3cf64fbfdcaee319f1e13f9457e1de0a6fa
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2021-04-26 18:31:02 +0200

    Wiki task

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.getWiki)
+    path("wiki/<str:name>", views.getWiki),
+    path("newPage/", views.newPage, name="newPage"),
+    path("editPage/", views.editPage, name="editPage"),
+    path("random/", views.random_page, name="random"),
 ]
--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -2,17 +2,57 @@ from django.shortcuts import render
 
 from . import util
 import markdown
+import logging
+import random
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    entries = util.list_entries()
+    if request.GET.get('q', '') != '':
+        if request.GET.get('q', '').upper() in entries:
+            entry = markdown.markdown(util.get_entry(request.GET.get('q', '')))
+            return render(request, "encyclopedia/entry.html", {
+                "entry": entry,
+                "name": request.GET.get('q', '')
+            })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": entries,
+        })
 
 
 def getWiki(request, name):
-    entry = markdown.markdown(util.get_entry(name))
+    entries = util.list_entries()
+    if request.GET.get('q', '') != '':
+        if request.GET.get('q', '').upper() in entries:
+            entry = markdown.markdown(util.get_entry(request.GET.get('q', '')))
+            return render(request, "encyclopedia/entry.html", {
+                "entry": entry,
+                "name": request.GET.get('q', '')
+            })
+    else:
+        entry = markdown.markdown(util.get_entry(name))
+        return render(request, "encyclopedia/entry.html", {
+            "entry": entry,
+            "name": name
+        })
+
+
+def newPage(request):
+    return render(request, "encyclopedia/new.html")
+
+
+def editPage(request):
+    return render(request, "encyclopedia/edit.html")
+
+
+def random_page(request):
+    entries = util.list_entries()
+    selected_page = random.choice(entries)
+    entry = markdown.markdown(util.get_entry(selected_page))
     return render(request, "encyclopedia/entry.html", {
         "entry": entry,
-        "name": name
+        "name": selected_page
     })
